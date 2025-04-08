268d456ff3144da2a744f05e934898a68463b3ea
Joe Glombek <joe@glombek.com>
2025-04-08 16:46:13 +0100

    Remove footer

--- global-top.vue
+++ global-top.vue
@@ -1,7 +1,7 @@
-<template>
+<!-- <template>
     <footer class="px-7 py-2 columns-3">
         <div class="text-left">Joe Glombek</div>
         <div class="text-center"><logos-mastodon-icon /> @joe@umbraco&#8239;community.social&emsp;<logos-bluesky /> @joe.gl</div>
         <div class="text-right">🔗 joe.gl/ombek</div>
     </footer>
-</template>
+</template> -->
